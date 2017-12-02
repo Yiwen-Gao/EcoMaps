26bf178d8acd13b0dbef5862eb65e78395fcc1b1
serenac26 <serenacheng99@gmail.com>
2017-12-02 15:23:02 -0500

    Multiple route options added

--- public/ecoMaps.js
+++ public/ecoMaps.js
@@ -2,7 +2,7 @@
                       Team 2
 /*-------------------------------------------------*/
 function initMap() {
-	var markerArray = [];
+	var directionDisplayArray = [];
 
 	// Instantiate a directions service.
 	var directionsService = new google.maps.DirectionsService;
@@ -31,20 +31,17 @@ function initMap() {
 	  // Browser doesn't support Geolocation
 	  handleLocationError(false, infoWindow, map.getCenter());
 	}
-
-	// Create a renderer for directions and bind it to the map.
-	var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
-
+	
 	// Instantiate an info window to hold step text.
 	var stepDisplay = new google.maps.InfoWindow;
 
 	// Display the route between the initial start and end selections.
-	calculateAndDisplayRoute(
-		directionsDisplay, directionsService, markerArray, stepDisplay, map);
+	calculateAndDisplayRoutes(
+		directionsService, directionDisplayArray, stepDisplay, map);
 	// Listen to change events from the start and end lists.
 	var onChangeHandler = function() {
-	  calculateAndDisplayRoute(
-		  directionsDisplay, directionsService, markerArray, stepDisplay, map);
+	  calculateAndDisplayRoutes(
+		  directionsService, directionDisplayArray, stepDisplay, map);
 	};
 	document.getElementById('start').addEventListener('change', onChangeHandler);
 	document.getElementById('end').addEventListener('change', onChangeHandler);
@@ -58,8 +55,8 @@ function initMap() {
 	infoWindow.open(map);
   }
 
-  function calculateAndDisplayRoute(directionsDisplay, directionsService,
-	  markerArray, stepDisplay, map) {
+  function calculateAndDisplayRoutes(directionsService,
+	  directionDisplayArray, stepDisplay, map) {
 	  if(document.getElementById('start').value == ""){
 		return;
 	  }
@@ -67,8 +64,8 @@ function initMap() {
 		return;
 	  }
 	// First, remove any existing markers from the map.
-	for (var i = 0; i < markerArray.length; i++) {
-	  markerArray[i].setMap(null);
+	for (var i = 0; i < directionDisplayArray.length; i++) {
+	  directionDisplayArray[i].setMap(null);
 	}
 
 	// Retrieve the start and end locations and create a DirectionsRequest using
@@ -76,15 +73,31 @@ function initMap() {
 	directionsService.route({
 	  origin: document.getElementById('start').value,
 	  destination: document.getElementById('end').value,
-	  travelMode: 'WALKING'	//user input
+	  travelMode: 'TRANSIT',//user input
+	  provideRouteAlternatives: true
 	}, function(response, status) {
 	  // Route the directions and pass the response to a function to create
 	  // markers for each step.
+	  var polylineColors = ["blue", "green", "red", "yellow", "purple", "orange", "gray", "black"];
 	  if (status === 'OK') {
-		document.getElementById('warnings-panel').innerHTML =
-			'<b>' + response.routes[0].warnings + '</b>';
-		directionsDisplay.setDirections(response);
-		//showSteps(response, markerArray, stepDisplay, map);
+		var numRoutes = response.routes.length;
+		console.log(numRoutes);
+		for(var i = 0; i < numRoutes; i++){
+			// Create a renderer for directions and bind it to the map.
+			var directionsDisplay = new google.maps.DirectionsRenderer({map: map, polylineOptions: polylineOptionsActual});
+			directionDisplayArray.push(directionsDisplay);
+			var polylineOptionsActual = {
+			  strokeColor: polylineColors[i],
+			  strokeOpacity: 1.0,
+			  strokeWeight: 10
+			 };
+			 document.getElementById('warnings-panel').innerHTML =
+			'<b>' + response.routes[i].warnings + '</b>';
+			directionsDisplay.setDirections(response);
+			directionsDisplay.setRouteIndex(i);
+		}
+		
+		//showSteps(response, directionDisplayArray, stepDisplay, map);
 	  } else {
 		window.alert('Directions request failed due to ' + status);
 	  }
