3bcb466ca474d2be9456dab56c4bd19d899105a6
serenac26 <serenacheng99@gmail.com>
2017-12-03 04:19:09 -0500

    fixed sidebar

--- public/structure.js
+++ public/structure.js
@@ -1,8 +1,7 @@
-var leafHTML = '<div class="leaf_rating" style="width: 3%;"> <image width="10" height="10" src="EcoLeaf.png"> </div>'
-var carHTML = '<i class="material-icons prefix">directions_car</i>';
-var railHTML = '<i class="material-icons prefix">directions_railway</i>';
+var leafHTML = '<div class="leaf_rating" style="width: 3%;"> <image width="10" height="10" src="EcoLeaf.png"> </div> ';
+var carHTML = '<i class="material-icons prefix">directions_car</i> ';
+var railHTML = '<i class="material-icons prefix">directions_railway</i> ';
 
-var route_list_element = document.getElementById("sidebar_route_lists");
 var styling = "";
 
 function addRoute(mode, response, cost) {
@@ -10,12 +9,11 @@ function addRoute(mode, response, cost) {
 	var iconHTML;
 	if (mode == 'DRIVING') iconHTML = carHTML;
 	else if (mode == 'TRANSIT') iconHTML = railHTML;
-	console.log(iconHTML);
 
-	styling += "<div>";
+	styling += "<div> ";
 	for (var i = 0 ; i < response.routes.length; i++) {
 		styling += iconHTML;
-		styling += '<div class="response_name"> <h3>' + i + 'm </h3> </div>';
+		styling += '<div class="route_name"> <h3>' + i + 'm</h3> </div> ';
 		
 		var costString = cost[i] + "";
 		var size = costString.substring(0,".");
@@ -24,7 +22,8 @@ function addRoute(mode, response, cost) {
 		else if (size ==3) styling += leafHTML;
 		else styling += "";
 
-		styling += '</div>';
 	}
-route_list_element.innnerHtml = styling;
+	styling += '</div>';
+	document.getElementById("sidebar_route_lists").innerHTML = styling;
+	console.log(styling);
 }
