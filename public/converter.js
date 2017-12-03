e68581bcc9300f68d50c1762791962e77583c371
Yiwen-Gao <yg2545@columbia.edu>
2017-12-03 00:42:55 -0500

    commented out everything in eco and added subway to converter

--- public/converter.js
+++ public/converter.js
@@ -1,17 +1,24 @@
 //From the EPA
-function convertAuto(jsonObject) {
-	double carbonEmittedKg = jsonObject.distance * 19.6 / jsonObject.mpg * 0.453592;
+function convertAuto(distance) {
+	double carbonEmittedKg = distance * 0.000621371 * 19.6 / jsonObject.mpg * 0.453592;
 	double cost = carbonEmitted * 0.001 * 10;
 	return cost;
 }
 
-function convertWalk(jsonObject) {
+function convertWalk(distance) {
 	return 0;
 }
 
 //Diesel hybrid electric buses have good gas mileage
-function convertBus(jsonObject) {
-	double carbonEmittedKg = 0.2 * convertAuto(jsonObject);
+function convertBus(distance) {
+	double carbonEmittedKg = 0.2 * convertAuto(distance);
 	double cost = carbonEmittedKg * 0.001 * 10;
 	return cost;
+}
+
+//From Carbon Footprint
+function convertSubway(distance) {
+	double carbonEmittedKg = distance * 0.000047;
+	double cost = carbonEmittedKg * 0.001 * 10; 
+	return cost
 }
